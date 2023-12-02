a18022a88b11c60c71c500c86cebe223857834ff
thunderbiscuit <thunderbiscuit@protonmail.com>
2023-12-02 14:21:31 -0500

    build: use the compose bill of materials 2023.06.01

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -44,20 +44,23 @@ android {
 
 dependencies {
     // basic android dependencies
-    implementation ("org.jetbrains.kotlin:kotlin-stdlib:1.9.21")
+    implementation ("org.jetbrains.kotlin:kotlin-stdlib:1.9.20")
     implementation ("androidx.core:core-ktx:1.12.0")
     implementation ("com.google.android.material:material:1.10.0")
 
     // compose
-    implementation("androidx.activity:activity-compose:1.8.1")
-    implementation("androidx.compose.material:material:1.1.1")
-    implementation("androidx.compose.animation:animation:1.1.1")
-    implementation("androidx.compose.ui:ui-tooling:1.1.1")
-    implementation("androidx.compose.runtime:runtime-livedata:1.1.1")
+    // Adding the Bill of Materials synchronizes dependencies in the androidx.compose namespace
+    // You can remove the library version in your dependency declarations
+    implementation(platform("androidx.compose:compose-bom:2023.06.01"))
+    implementation("androidx.compose.material:material")
+    implementation("androidx.compose.animation:animation")
+    implementation("androidx.compose.ui:ui-tooling")
+    implementation("androidx.compose.runtime:runtime-livedata")
+    implementation("androidx.compose.ui:ui")
+    implementation("androidx.compose.material3:material3")
+    implementation("androidx.activity:activity-compose")
     implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.4.1")
     implementation("androidx.navigation:navigation-compose:2.4.1")
-    implementation("androidx.compose.ui:ui:1.1.1")
-    implementation("androidx.compose.material3:material3:1.1.2")
     implementation("androidx.constraintlayout:constraintlayout-compose:1.0.1")
     implementation("com.google.accompanist:accompanist-navigation-animation:0.23.1")
     implementation("com.google.accompanist:accompanist-systemuicontroller:0.23.1")
