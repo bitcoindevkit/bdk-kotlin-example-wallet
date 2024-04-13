d20e29368dfe0433b96c5462740015bc88a5926a
thunderbiscuit <thunderbiscuit@protonmail.com>
2024-04-12 22:24:08 -0400

    feat: add protobuf extension functions

---
+++ app/src/main/java/org/bitcoindevkit/devkitwallet/utils/ProtobufExtensions.kt
@@ -0,0 +1,27 @@
+/*
+ * Copyright 2020-2024 thunderbiscuit and contributors.
+ * Use of this source code is governed by the Apache 2.0 license that can be found in the ./LICENSE file.
+ */
+
+package org.bitcoindevkit.devkitwallet.utils
+
+import org.bitcoindevkit.Network
+import org.bitcoindevkit.devkitwallet.data.ActiveWalletNetwork
+
+fun Network.intoProto(): ActiveWalletNetwork {
+    return when (this) {
+        Network.TESTNET -> ActiveWalletNetwork.TESTNET
+        Network.SIGNET -> ActiveWalletNetwork.SIGNET
+        Network.REGTEST -> ActiveWalletNetwork.REGTEST
+        Network.BITCOIN -> throw IllegalArgumentException("Bitcoin mainnet network is not supported")
+    }
+}
+
+fun ActiveWalletNetwork.intoDomain(): Network {
+    return when (this) {
+        ActiveWalletNetwork.TESTNET -> Network.TESTNET
+        ActiveWalletNetwork.SIGNET -> Network.SIGNET
+        ActiveWalletNetwork.REGTEST -> Network.REGTEST
+        ActiveWalletNetwork.UNRECOGNIZED -> throw IllegalArgumentException("Unrecognized network")
+    }
+}
